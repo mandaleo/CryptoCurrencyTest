c32f8ea290d548a289becea46a7b52274d279816
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2018-06-03 23:24:02 +0200

    Add firs desing CurrencyDetailView

--- CryptoCurrencyTest/Controllers/CurrencyDetail/CurrencyDetailView.swift
+++ CryptoCurrencyTest/Controllers/CurrencyDetail/CurrencyDetailView.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class CurrencyDetailView: UIView {
+    
+    //MARK: - Outlets
+    @IBOutlet fileprivate weak var nameLabel: UILabel!
+    @IBOutlet fileprivate weak var symbolLabel: UILabel!
+    @IBOutlet fileprivate weak var rankLabel: UILabel!
+    @IBOutlet fileprivate weak var usdPriceLabel: UILabel!
+    @IBOutlet fileprivate weak var btcPriceLabel: UILabel!
+    @IBOutlet fileprivate weak var usdVolumeLastDayLabel: UILabel!
+    @IBOutlet fileprivate weak var usdCapMarketLabel: UILabel!
+    @IBOutlet fileprivate weak var availableSupplyLabel: UILabel!
+    @IBOutlet fileprivate weak var totalSupplyLabel: UILabel!
+    @IBOutlet fileprivate weak var percentageChangeLastHourLabel: UILabel!
+    @IBOutlet fileprivate weak var percentageChangeLastDayLabel: UILabel!
+    @IBOutlet fileprivate weak var percentageChangeLastWeekLabel: UILabel!
+    
 
     //MARK: - Variables
     fileprivate var viewModel: CurrencyViewModel?
@@ -20,5 +35,17 @@ class CurrencyDetailView: UIView {
     
     func presentViewModel(vm: CurrencyViewModel) {
         viewModel = vm
+        nameLabel.text = vm.name
+        symbolLabel.text = vm.symbol
+        rankLabel.text = "\(vm.rank)"
+        usdPriceLabel.text = "\(vm.priceUSD)"
+        btcPriceLabel.text = "\(vm.priceBTC)"
+        usdVolumeLastDayLabel.text = "\(vm.volumeUSD)"
+        usdCapMarketLabel.text = "\(vm.marketCapUSD)"
+        availableSupplyLabel.text = "\(vm.availableSupply)"
+        totalSupplyLabel.text = "\(vm.totalSupply)"
+        percentageChangeLastHourLabel.text = "\(vm.percentageLastHour)"
+        percentageChangeLastDayLabel.text = "\(vm.percentageLastDay)"
+        percentageChangeLastWeekLabel.text = "\(vm.percentageLastWeek)"
     }
 }
